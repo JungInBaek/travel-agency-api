0880157c8e0cc2519f7b70dd9cad3459687fa2d8
JungInbaek <bji1527@naver.com>
2023-07-17 19:12:54 +0900

    refact : MemberRepository 코드 수정
    - @Transactional 설정
    - Optional.of()로 변경

--- src/main/java/com/travel/agency/repository/MemberRepository.java
+++ src/main/java/com/travel/agency/repository/MemberRepository.java
@@ -6,9 +6,11 @@ import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 @Repository
+@Transactional
 @RequiredArgsConstructor
 public class MemberRepository {
 
@@ -18,14 +20,15 @@ public class MemberRepository {
         em.persist(member);
     }
 
+    @Transactional(readOnly = true)
     public Optional<Member> findById(String id) {
-        return em.createQuery("select m from Member m where m.id = :id", Member.class)
+        return Optional.of(em.createQuery("select m from Member m where m.id = :id", Member.class)
                 .setParameter("id", id)
-                .getResultStream().findFirst();
+                .getSingleResult());
     }
 
     public void deleteById(String id) {
-        Assert.notNull(id, "The given member id must not be null!");
+        Assert.notNull(id, "The given delete member id must not be null!");
 
         delete(findById(id).orElseThrow(() -> new EmptyResultDataAccessException(
                 String.format("No %s entity with id %s exists!", Member.class, id), 1)));
@@ -34,7 +37,6 @@ public class MemberRepository {
     public void delete(Member member) {
         Assert.notNull(member, "Entity must not be null!");
 
-        // if the entity to be deleted doesn't exist, delete is a NOOP
         if (member == null) {
             return;
         }
